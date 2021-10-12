ead80551bc869eb82f5fe7fee63d190312ae2f43
Bryan-Jeetun <bryan-jeetun@student.ucll.be>
2021-10-12 12:56:12 +0000

    Added automatic backup every 15mins

--- scripts/incron_config.sh
+++ scripts/incron_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-*/15 * * * * find /etc -type d ! -regex '.*/.git.*' -print0 | xargs -0 -I{} echo "{} IN_CLOSE_WRITE,recursive=false /etc/scripts/backup.sh \$@/\$#" > /etc/incron.d/etc.conf
-*/15 * * * * systemctl restart incron
+find /etc -type d ! -regex '.*/.git.*' -print0 | xargs -0 -I{} echo "{} IN_CLOSE_WRITE,recursive=false /etc/scripts/backup.sh \$@/\$#" > /etc/incron.d/etc.conf
+systemctl restart incron
